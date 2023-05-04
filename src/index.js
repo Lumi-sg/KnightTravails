9f8f0c854b2c2d266d1150cc387d534a49acf98b
Lumi <shaicloud@gmail.com>
2023-05-04 02:57:29 -0400

    initial functionality, needs lots of refactor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
-console.log("Hello World!");
+const body = (document.querySelector("body").style.backgroundColor = "black");
+
+class Knight {
+	constructor(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+}
+function getLegalMoves(knight) {
+	const offsets = [
+		[-2, -1],
+		[-2, 1],
+		[2, -1],
+		[2, 1],
+		[-1, -2],
+		[-1, 2],
+		[1, -2],
+		[1, 2],
+	];
+
+	const moves = [];
+
+	for (const [dx, dy] of offsets) {
+		const x = knight.x + dx;
+		const y = knight.y + dy;
+
+		if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+			moves.push(new Knight(x, y));
+		}
+	}
+
+	return moves;
+}
+function knightTravail(start, end) {
+	const queue = [];
+	const visited = new Set();
+
+	queue.push([start, 0]);
+	visited.add(start.x + "," + start.y);
+
+	while (queue.length > 0) {
+		const [knight, depth] = queue.shift();
+
+		if (knight.x === end.x && knight.y === end.y) {
+			return depth;
+		}
+
+		const moves = getLegalMoves(knight);
+
+		for (const move of moves) {
+			const key = move.x + "," + move.y;
+
+			if (!visited.has(key)) {
+				queue.push([move, depth + 1]);
+				visited.add(key);
+			}
+		}
+	}
+}
+const start = new Knight(0, 0);
+const end = new Knight(7, 7);
+const result = knightTravail(start, end);
+
+console.log(result);
